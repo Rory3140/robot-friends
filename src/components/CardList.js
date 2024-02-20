a798ede987df5787df914fd6736c8344f85bf271
Rory Wood <98532160+Rory3140@users.noreply.github.com>
2024-02-20 11:36:11 -0500

    removed error statement test

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,9 +2,6 @@ import React from "react";
 import Card from "./Card";
 
 const CardList = ({ robots }) => {
-  if (true) {
-    throw new Error("NO");
-  }
   return (
     <div className="tc">
       {robots.map((robot, i) => {
